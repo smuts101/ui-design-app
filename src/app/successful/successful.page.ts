5f0e3c3b38d1f4562c73874235108ebb36fde543
smuts101 <69514780+smuts101@users.noreply.github.com>
2020-10-17 15:50:47 +0200

    ui completed

---
+++ src/app/successful/successful.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-successful',
+  templateUrl: './successful.page.html',
+  styleUrls: ['./successful.page.scss'],
+})
+export class SuccessfulPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
